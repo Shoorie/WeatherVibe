5b594805651e59ac74dba3ffc16ca1cd9a78b826
Adrian Zalewski <adrian.zalewski@empik.com>
2026-03-22 17:45:07 +0100

    Update compose-ui - resources section

--- feature/home/src/main/kotlin/com/weather/vibe/feature/home/presentation/HomeViewModel.kt
+++ feature/home/src/main/kotlin/com/weather/vibe/feature/home/presentation/HomeViewModel.kt
@@ -3,11 +3,12 @@ package com.weather.vibe.feature.home.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.weather.vibe.domain.weather.usecase.GetWeather
-import com.weather.vibe.feature.home.presentation.HomeAction.LocationResultReceived
+import com.weather.vibe.feature.home.presentation.HomeAction.ReceiveLocationResult
 import com.weather.vibe.feature.home.presentation.HomeAction.RefreshClick
 import com.weather.vibe.feature.home.presentation.state.HomeUiState
 import com.weather.vibe.feature.home.presentation.state.HomeUiState.Error
 import com.weather.vibe.feature.home.presentation.state.HomeUiState.Loading
+import com.weather.vibe.feature.home.ui.HomeResources
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -19,6 +20,7 @@ import org.koin.android.annotation.KoinViewModel
 @KoinViewModel
 internal class HomeViewModel(
   private val getWeather: GetWeather,
+  private val resources: HomeResources,
   private val stateFactory: HomeStateFactory
 ) : ViewModel() {
 
@@ -31,12 +33,12 @@ internal class HomeViewModel(
 
   fun dispatch(action: HomeAction) {
     when (action) {
-      is LocationResultReceived -> onLocationResultReceived(action)
+      is ReceiveLocationResult -> onReceiveLocationResult(action)
       is RefreshClick -> onRefreshClick()
     }
   }
 
-  private fun onLocationResultReceived(action: LocationResultReceived) {
+  private fun onReceiveLocationResult(action: ReceiveLocationResult) {
     loadWeather(action.latitude, action.longitude, action.cityName)
   }
 
@@ -55,7 +57,7 @@ internal class HomeViewModel(
         _state.update {
           result.fold(
             onSuccess = stateFactory::create,
-            onFailure = { Error(it.message ?: DEFAULT_ERROR) }
+            onFailure = { Error(it.message ?: resources.defaultError()) }
           )
         }
       }
@@ -64,7 +66,6 @@ internal class HomeViewModel(
 
   private companion object {
     const val DEFAULT_CITY = "Toruń"
-    const val DEFAULT_ERROR = "Unexpected error"
     const val DEFAULT_LATITUDE = 53.0138
     const val DEFAULT_LONGITUDE = 18.5984
   }

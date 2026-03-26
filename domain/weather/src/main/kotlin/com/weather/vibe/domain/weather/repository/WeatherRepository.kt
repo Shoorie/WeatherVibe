f4cec8c5fa7caadf2087a51bc2a8cc771bcc762b
Adrian Zalewski <adrian.zalewski@empik.com>
2026-03-26 07:56:59 +0100

    Update formatting

--- domain/weather/src/main/kotlin/com/weather/vibe/domain/weather/repository/WeatherRepository.kt
+++ domain/weather/src/main/kotlin/com/weather/vibe/domain/weather/repository/WeatherRepository.kt
@@ -3,7 +3,12 @@ package com.weather.vibe.domain.weather.repository
 import com.weather.vibe.domain.weather.model.WeatherData
 
 interface WeatherRepository {
-  suspend fun getCurrentTemperature(latitude: Double, longitude: Double): Double
+
+  suspend fun getCurrentTemperature(
+    latitude: Double,
+    longitude: Double
+  ): Double
+
   suspend fun getWeather(
     latitude: Double,
     longitude: Double,

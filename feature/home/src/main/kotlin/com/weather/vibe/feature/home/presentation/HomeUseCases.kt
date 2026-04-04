74c79e3802c7798298387c4c1dfb123cff81eaae
Shoorie <adrianzalewski03@gmail.com>
2026-04-04 21:58:59 +0200

    Aggregate use cases in Home and Search ViewModels
    Group use case dependencies into dedicated HomeUseCases and SearchUseCases
    aggregator classes to reduce ViewModel constructor bloat.
    
    - HomeViewModel: 8 params -> 3 params (6 use cases aggregated)
    - SearchViewModel: 5 params -> 2 params (4 use cases aggregated)

---
+++ feature/home/src/main/kotlin/com/weather/vibe/feature/home/presentation/HomeUseCases.kt
@@ -0,0 +1,19 @@
+package com.weather.vibe.feature.home.presentation
+
+import com.weather.vibe.domain.settings.usecase.ObserveUserSettings
+import com.weather.vibe.domain.settings.usecase.SaveUserSettings
+import com.weather.vibe.domain.weather.usecase.ComputeWeatherKey
+import com.weather.vibe.domain.weather.usecase.GenerateWeatherSuggestion
+import com.weather.vibe.domain.weather.usecase.GetWeather
+import com.weather.vibe.domain.weather.usecase.InvalidateWeatherSuggestion
+import org.koin.core.annotation.Factory
+
+@Factory
+internal class HomeUseCases(
+  val computeWeatherKey: ComputeWeatherKey,
+  val generateWeatherSuggestion: GenerateWeatherSuggestion,
+  val getWeather: GetWeather,
+  val invalidateWeatherSuggestion: InvalidateWeatherSuggestion,
+  val observeUserSettings: ObserveUserSettings,
+  val saveUserSettings: SaveUserSettings,
+)

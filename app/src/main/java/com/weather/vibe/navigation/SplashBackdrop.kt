697d4b220db891156f391fb536e26094ee102e8f
Shoorie <adrianzalewski03@gmail.com>
2026-04-15 22:42:43 +0200

    Update import

--- app/src/main/java/com/weather/vibe/navigation/SplashBackdrop.kt
+++ app/src/main/java/com/weather/vibe/navigation/SplashBackdrop.kt
@@ -5,13 +5,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.weather.vibe.core.designsystem.theme.WeatherVibeTheme
+import com.weather.vibe.core.designsystem.theme.WeatherVibeTheme.colors
 
 @Composable
 internal fun SplashBackdrop() {
   Box(
     modifier = Modifier
       .fillMaxSize()
-      .background(WeatherVibeTheme.colors.backgroundGradientStart)
+      .background(colors.backgroundGradientStart)
   )
 }

7faad3476002995d24fe154c235ae73d7b72fe0e
Shoorie <adrianzalewski03@gmail.com>
2026-04-17 22:59:20 +0200

    Apply kover plugin to test-bearing modules

--- data/settings/build.gradle.kts
+++ data/settings/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   alias(libs.plugins.weathervibe.android.library)
   alias(libs.plugins.weathervibe.android.koin)
   alias(libs.plugins.weathervibe.android.datastore)
+  alias(libs.plugins.weathervibe.android.kover)
   alias(libs.plugins.weathervibe.android.test)
 }
 

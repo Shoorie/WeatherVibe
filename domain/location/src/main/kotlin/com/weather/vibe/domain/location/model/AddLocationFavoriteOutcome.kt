3d8e6c5f8ac371ddef57df0c23ae9004fd572ba0
Shoorie <adrianzalewski03@gmail.com>
2026-04-23 21:13:57 +0200

    Move add-favorite atomicity to repo transaction, drop Outcome type

--- domain/location/src/main/kotlin/com/weather/vibe/domain/location/model/AddLocationFavoriteOutcome.kt
+++
@@ -1,7 +0,0 @@
-package com.weather.vibe.domain.location.model
-
-sealed interface AddLocationFavoriteOutcome {
-  data object Added : AddLocationFavoriteOutcome
-  data object AlreadyExists : AddLocationFavoriteOutcome
-  data object LimitReached : AddLocationFavoriteOutcome
-}

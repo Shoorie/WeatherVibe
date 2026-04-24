8b86ef429050ee75644e287ebe2be85e6172d8b3
Shoorie <adrianzalewski03@gmail.com>
2026-04-24 16:56:40 +0200

    Quick wins: drop dead VibeHistoryRange, add flowOn(IO), tidy imports

--- feature/viberating/src/main/kotlin/com/weather/vibe/feature/viberating/presentation/history/VibeHistoryAction.kt
+++ feature/viberating/src/main/kotlin/com/weather/vibe/feature/viberating/presentation/history/VibeHistoryAction.kt
@@ -1,7 +1,6 @@
 package com.weather.vibe.feature.viberating.presentation.history
 
 import java.time.LocalDate
-import java.time.YearMonth
 
 internal sealed interface VibeHistoryAction {
 
@@ -14,11 +13,4 @@ internal sealed interface VibeHistoryAction {
   data object DayDetailDismissed : VibeHistoryAction
 
   data object BackClick : VibeHistoryAction
-}
-
-internal data class VibeHistoryRange(
-  val viewMonth: YearMonth,
-  val currentMonth: YearMonth
-) {
-  val canNavigateNext: Boolean = viewMonth < currentMonth
 }

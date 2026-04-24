c4bb664979783826edd3de3e8fe273a31baa3823
Shoorie <adrianzalewski03@gmail.com>
2026-04-24 11:58:30 +0200

    Remove debug favorites seeder

--- app/src/main/java/com/weather/vibe/WeatherVibeApplication.kt
+++ app/src/main/java/com/weather/vibe/WeatherVibeApplication.kt
@@ -1,20 +1,13 @@
 package com.weather.vibe
 
 import android.app.Application
-import android.content.pm.ApplicationInfo
 import android.util.Log
 import androidx.work.Configuration
 import com.weather.vibe.core.tracing.TraceSections.KOIN_INITIALIZATION
 import com.weather.vibe.core.tracing.traceSection
-import com.weather.vibe.dev.LocationFavoritesSeeder
 import com.weather.vibe.di.WeatherVibeApp
 import com.weather.vibe.feature.widget.work.WidgetRefreshCoordinator
 import com.weather.vibe.scheduling.SchedulingCoordinator
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
 import org.koin.android.ext.android.get
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -40,21 +33,5 @@ class WeatherVibeApplication : Application(), Configuration.Provider {
     }
     get<WidgetRefreshCoordinator>().start()
     get<SchedulingCoordinator>().start()
-    if (isDebuggable) seedDummyFavoritesIfEmpty()
-  }
-
-  private val isDebuggable: Boolean
-    get() = (applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) != 0
-
-  private fun seedDummyFavoritesIfEmpty() {
-    val seeder = get<LocationFavoritesSeeder>()
-    val handler = CoroutineExceptionHandler { _, throwable ->
-      Log.w(TAG_SEEDER, "Dummy favorites seed failed", throwable)
-    }
-    CoroutineScope(SupervisorJob() + IO + handler).launch { seeder.seedIfEmpty() }
-  }
-
-  private companion object {
-    const val TAG_SEEDER = "LocationFavoritesSeeder"
   }
 }

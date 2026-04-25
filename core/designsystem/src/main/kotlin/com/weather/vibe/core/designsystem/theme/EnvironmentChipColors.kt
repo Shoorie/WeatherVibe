33bbd484685fb91d4faa88816586fbed3e78e179
Shoorie <adrianzalewski03@gmail.com>
2026-04-25 19:32:06 +0200

    Rename BriefAirChip to AirQualityChip and lift palette tokens to design system

---
+++ core/designsystem/src/main/kotlin/com/weather/vibe/core/designsystem/theme/EnvironmentChipColors.kt
@@ -0,0 +1,12 @@
+package com.weather.vibe.core.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+object EnvironmentChipColors {
+  val GreenContainer: Color = Color(0xFFDCFCE7)
+  val GreenContent: Color = Color(0xFF14532D)
+  val AmberContainer: Color = Color(0xFFFEF3C7)
+  val AmberContent: Color = Color(0xFF78350F)
+  val RoseContainer: Color = Color(0xFFFEE2E2)
+  val RoseContent: Color = Color(0xFF7F1D1D)
+}

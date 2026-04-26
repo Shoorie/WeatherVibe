11d0f306d1c80a5943455918ddd910ffcd196e74
Shoorie <adrianzalewski03@gmail.com>
2026-04-26 10:42:13 +0200

    Penalize night, blocking conditions, gusts and use apparent temp

--- domain/activityplanner/src/main/kotlin/com/weather/vibe/domain/activityplanner/model/ScoreReason.kt
+++ domain/activityplanner/src/main/kotlin/com/weather/vibe/domain/activityplanner/model/ScoreReason.kt
@@ -1,5 +1,7 @@
 package com.weather.vibe.domain.activityplanner.model
 
+import com.weather.vibe.domain.weather.model.WeatherCondition
+
 sealed interface ScoreReason {
   data class TemperatureOptimal(val celsius: Double) : ScoreReason
   data class TemperatureTooHigh(val celsius: Double) : ScoreReason
@@ -8,6 +10,12 @@ sealed interface ScoreReason {
   data class UvHigh(val index: Double) : ScoreReason
   data class WindCalm(val kmh: Double) : ScoreReason
   data class WindStrong(val kmh: Double) : ScoreReason
+  data class GustsCalm(val kmh: Double) : ScoreReason
+  data class GustsStrong(val kmh: Double) : ScoreReason
   data class PrecipitationDry(val probability: Int) : ScoreReason
   data class PrecipitationLikely(val probability: Int) : ScoreReason
+  data object Daylight : ScoreReason
+  data object NightHours : ScoreReason
+  data class ConditionFriendly(val condition: WeatherCondition) : ScoreReason
+  data class ConditionBlocking(val condition: WeatherCondition) : ScoreReason
 }
--- domain/activityplanner/src/main/kotlin/com/weather/vibe/domain/activityplanner/usecase/ScoreHourForActivity.kt
+++ domain/activityplanner/src/main/kotlin/com/weather/vibe/domain/activityplanner/usecase/ScoreHourForActivity.kt
@@ -5,6 +5,7 @@ import com.weather.vibe.domain.activityplanner.model.ActivityType
 import com.weather.vibe.domain.activityplanner.model.ScoredHour
 import com.weather.vibe.domain.weather.model.HourlyWeather
 import org.koin.core.annotation.Factory
+import java.time.LocalDateTime
 
 @Factory
 class ScoreHourForActivity(
@@ -15,12 +16,26 @@ class ScoreHourForActivity(
   operator fun invoke(
     hour: HourlyWeather,
     uvIndex: Double,
+    sunrise: LocalDateTime?,
+    sunset: LocalDateTime?,
     activity: ActivityType
   ): ScoredHour {
 
     val preferences = forActivity(activity)
-    val penalty = calculateHourPenalty(hour, uvIndex, preferences)
-    val reasons = collectScoreReasons(hour, uvIndex, preferences)
+    val penalty = calculateHourPenalty(
+      hour = hour,
+      uvIndex = uvIndex,
+      sunrise = sunrise,
+      sunset = sunset,
+      preferences = preferences
+    )
+    val reasons = collectScoreReasons(
+      hour = hour,
+      uvIndex = uvIndex,
+      sunrise = sunrise,
+      sunset = sunset,
+      preferences = preferences
+    )
 
     return ScoredHour(
       time = hour.time,

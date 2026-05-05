a18bb2f6e9695b008e675c633993f8586277a688
Shoorie <adrianzalewski03@gmail.com>
2026-05-05 20:51:10 +0200

    Drop stale dp import in VibeButtonStack

--- core/designsystem/src/main/kotlin/com/weather/vibe/core/designsystem/components/button/VibeButtonStack.kt
+++ core/designsystem/src/main/kotlin/com/weather/vibe/core/designsystem/components/button/VibeButtonStack.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
-import androidx.compose.ui.unit.dp
 import com.weather.vibe.core.designsystem.components.button.VibeButtonStackDefaults.PrimaryToSecondaryGap
 import com.weather.vibe.core.designsystem.theme.WeatherVibeTheme
 import com.weather.vibe.core.designsystem.theme.WeatherVibeTheme.colors

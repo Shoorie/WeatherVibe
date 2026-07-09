26bb958fa380e77ac25b5cb4e917cb4d9fd45a49
Shoorie <shoorie@users.noreply.github.com>
2026-07-09 14:43:35 +0200

    [WV-2] Sandbox fixture for release/1.3

---
+++ dependencies.gradle.kts
@@ -0,0 +1,9 @@
+val sandboxVersions = mapOf(
+    "api_version" to 122,
+    "koin" to "4.1.0",
+    "room" to "2.5.2",
+    "moshi" to "1.15.1",
+    "retrofit" to "2.9.0",
+    "okhttp" to "4.9.3",
+    "turbine" to "1.0.0"
+)
